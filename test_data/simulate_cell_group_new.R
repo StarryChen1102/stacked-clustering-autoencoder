339d22241b5797d73b9306b3ff0c66d69d384d61
StarryChen1102 <104817932+StarryChen1102@users.noreply.github.com>
2023-10-31 00:07:42 -0400

    Update test data

---
+++ test_data/simulate_cell_group_new.R
@@ -0,0 +1,56 @@
+library(splatter)
+library(Seurat)
+params <- newSplatParams(batchCells = 2000, nGenes = 5000, 
+                         group.prob = c(0.09,0.09,0.1,0.08,0.12,0.11,0.1,0.1,0.11,0.1),
+                         #dropout.type = "experiment",
+                         dropout.mid = 0.8,
+                         dropout.shape = -0.8,
+                         de.prob = 0.3,
+                         de.facScale = c(0.1,0.12,0.11,0.13,0.11,0.09,0.07,0.1,0.11,0.08),
+                         seed = 8)
+
+params <- newSplatParams(batchCells = 25000, nGenes = 5000, 
+                         group.prob = c(0.09,0.09,0.1,0.08,0.06,0.11,0.1,0.06,0.1,0.06,0.05,0.1),
+                         #dropout.type = "experiment",
+                         dropout.mid = 0,
+                         dropout.shape = -1,
+                         de.prob = 0.3,
+                         de.facScale = c(0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3),
+                         seed = 8)
+
+sim <- splatSimulateGroups(params, verbose = FALSE)
+simcounts <- counts(sim)
+idents <- sim$Group
+
+sim4 <- CreateSeuratObject(counts = counts(sim))
+Idents(sim4) <- sim$Group
+
+sim4 <- NormalizeData(sim4)
+sim4 <- FindVariableFeatures(sim4, selection.method = "vst")
+sim4 <- ScaleData(sim4)
+
+sim4 <- RunPCA(sim4)
+DimPlot(sim4, reduction = "pca")
+sim4 <- RunTSNE(sim4, dims = 1:10)
+DimPlot(sim4, reduction = "tsne")
+sim4 <- RunUMAP(sim4, dims = 1:20)
+DimPlot(sim4, reduction = "umap")
+
+write.table(idents, file='data3_dropout_idents_0.csv', quote=FALSE, sep='\t', col.names = NA)
+
+sim4 <- SCTransform(sim4, verbose = FALSE)
+write.table(sim4@assays$SCT@scale.data, file='data3_dropout_SCT_0.csv', quote=FALSE, sep='\t', col.names = TRUE, row.names = TRUE)
+
+write.table(t(simcounts), file='data3_raw.csv', quote=FALSE, sep='\t', col.names = NA)
+
+write.table(simcounts, file='data3_dropout_raw_0.csv', quote=FALSE, sep=',', col.names = TRUE, row.names = TRUE)
+
+normalized_data <- sctransform::vst(simcounts)$y
+write.table(idents, file='idents0_SCT2.csv', quote=FALSE, sep='\t', col.names = NA)
+write.table(t(normalized_data), file='data0_SCT2.csv', quote=FALSE, sep='\t', col.names = NA)
+
+
+
+
+
+
